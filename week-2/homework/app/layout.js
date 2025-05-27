b50498a0f21ece3bc45d27862cf28de47067c727
Aviv <aviv.dror@gmail.com>
2025-05-27 23:46:09 +0200

    nav item component

--- week-2/homework/app/layout.js
+++ week-2/homework/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Navbar } from "@/components/ui/Navbar";
+import { Navbar } from "@/components/ui/navBar/Navbar";
 import { Footer } from "@/components/ui/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
