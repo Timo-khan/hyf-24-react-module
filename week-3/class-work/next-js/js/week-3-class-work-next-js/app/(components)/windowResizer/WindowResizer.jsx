5d9513c647ad957b0bbfec366a497c76fc923b39
Aviv <aviv.dror@gmail.com>
2025-06-01 21:46:02 +0200

    window resizer

--- week-3/class-work/next-js/js/week-3-class-work-next-js/app/(components)/windowResizer/WindowResizer.jsx
+++ week-3/class-work/next-js/js/week-3-class-work-next-js/app/(components)/windowResizer/WindowResizer.jsx
@@ -8,17 +8,17 @@ export const WindowResizer = () => {
   const [windowSize, setWindowSize] = useState([0, 0]);
 
   useEffect(() => {
-    const updateWindowSize = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    };
     window.addEventListener("resize", updateWindowSize);
     updateWindowSize();
     return () => window.removeEventListener("resize", updateWindowSize);
   }, []);
 
+  const updateWindowSize = () => {
+    setWindowSize([window.innerWidth, window.innerHeight]);
+  };
+
   return (
     <div className="window-resizer-wrapper">
-      {/* <pre>{JSON.stringify(windowSize, null, 2)}</pre> */}
       <p className="window-resizer-head">Current Window Size :</p>
       <span className="window-resizer-size">
         {windowSize[0]} x {windowSize[1]}
